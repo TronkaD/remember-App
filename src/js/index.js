a8a67f27fd8125198e3566d9708bd9fb591f4b61
Salim Aouas <salimaouas@gmail.com>
2024-10-24 17:58:29 +0200

    fix newPath variable

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,28 @@
 const redirect = (view, isFromIndex) => {
-    const currentFullPath = window.location.pathname;
-    const currentRoute = currentFullPath.split('/').pop();
-    const baseURL = currentFullPath.replace(`/${currentRoute}`, "");
-    console.log('Vérification de sessionStorage...');
+  const currentFullPath = window.location.pathname;
+  const currentRoute = currentFullPath.split("/").pop();
+  const baseURL = currentFullPath.replace(`/${currentRoute}`, "");
 
-    if(isFromIndex) {
-        const newPath = `${baseURL}/src/views/${view}.html`;
-        window.location.href = newPath;
-        return;
-    }
-    console.log(currentRoute);
+  console.log("baseURL", baseURL);
+  console.log("Vérification de sessionStorage...");
+
+  if (isFromIndex) {
+    const newPath = `${baseURL}/views/${view}.html`;
+    window.location.href = newPath;
+    return;
+  }
+  console.log(currentRoute);
 };
 const changeView = (view) => {
-    if(view === 'login') {
-        //rediriger vers login
-        redirect('login', true);
-    }
+  if (view === "login") {
+    // rediriger vers login
+    redirect("login", true);
+  }
 
-    if(view === 'register') {
-        //rediriger vers register
-        redirect('register', true);
-    }
+  if (view === "register") {
+    // rediriger vers register
+    redirect("register", true);
+  }
 };
 
 window.changeView = changeView;
